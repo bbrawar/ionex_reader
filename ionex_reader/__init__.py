c0773803cff0c1b6d045a5a79804537559b26368
Bhuvnesh Brawar <90771976+bbrawar@users.noreply.github.com>
2024-07-02 14:16:16 +0530

    Create __init__.py

---
+++ ionex_reader/__init__.py
@@ -0,0 +1,10 @@
+from .ionex import get_tecmaps, parse_map, get_epoch, create_xarray, get_tec, plot_tec_map
+
+__all__ = [
+    'get_tecmaps',
+    'parse_map',
+    'get_epoch',
+    'create_xarray',
+    'get_tec',
+    'plot_tec_map'
+]
